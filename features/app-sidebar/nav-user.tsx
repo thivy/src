9641efbae330e267cc2b8863f087efc9b366f9c5
thivy <thivyakanth@gmail.com>
2025-07-01 10:01:44 +1000

    fix: remove unnecessary class from user login provider display

--- features/app-sidebar/nav-user.tsx
+++ features/app-sidebar/nav-user.tsx
@@ -59,7 +59,7 @@ export function UserDropdown({ user }: { user: User }) {
                 <>
                   <div className="grid flex-1 gap-1 text-left text-sm leading-tight ">
                     <span className="truncate font-medium">{user.name}</span>
-                    <span className="truncate text-xs text-muted-foreground">
+                    <span className="truncate text-xs ">
                       {user.loginProvider}
                     </span>
                   </div>
